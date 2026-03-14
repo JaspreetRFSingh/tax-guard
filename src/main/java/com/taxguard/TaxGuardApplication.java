74e19a7df18a8c37506b5c48a6e84200603d67bf
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2026-03-15 00:52:36 +0530

    added spring boot runner class

---
+++ src/main/java/com/taxguard/TaxGuardApplication.java
@@ -0,0 +1,11 @@
+package com.taxguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxGuardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaxGuardApplication.class, args);
+    }
+}
